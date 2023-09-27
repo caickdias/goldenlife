5765b3505f22adebfab317a4c132ce53fd88ff61
Caick Andrade <caickandrade0@gmail.com>
2023-09-27 15:56:34 -0300

    fran

--- src/components/HeroSection/Subtitle.jsx
+++
@@ -1,9 +0,0 @@
-const Subtitle = ({ title }) => {
-    return(
-        <h1 className='text-xl text-white lg:font-bold'>
-            {title}
-        </h1>
-    )
-}
-
-export default Subtitle;
--- src/data/speakers.js
+++
@@ -1,5 +0,0 @@
-import banner from '../assets/speakers.png';
-
-export {
-    banner,
-}
